fdac19aef739b0ff3deb85d0e966ea52b6eae279
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@62949778-16d8-8ee4-2210-6f5ccdf4390f>
2011-06-05 22:42:28 +0000

    mn27 feito

--- trunk/parte 1/gt10/gt10.sol
+++ trunk/parte 1/gt10/gt10.sol
@@ -1,58 +1,26 @@
-Problem:    gt10
-Rows:       22
-Columns:    5 (5 integer, 5 binary)
-Non-zeros:  58
+Problem:    Unknown
+Rows:       8
+Columns:    3 (3 integer, 3 binary)
+Non-zeros:  16
 Status:     INTEGER OPTIMAL
 Objective:  objetivo = 1 (MINimum)
 
    No.   Row name        Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 vizinhoA[1,2,4]
-                                   0                           1 
-     2 vizinhoA[1,2,3]
-                                   1                           1 
-     3 vizinhoA[1,4,2]
-                                   0                           1 
-     4 vizinhoA[1,4,3]
-                                   1                           1 
-     5 vizinhoA[1,3,2]
-                                   1                           1 
-     6 vizinhoA[1,3,4]
-                                   1                           1 
-     7 vizinhoB[2,3,1]
-                                   1                           1 
-     8 vizinhoB[3,4,1]
-                                   0                           1 
-     9 vizinhoB[1,4,3]
-                                   0                           1 
-    10 vizinhoB[1,3,2]
-                                   1                           1 
-    11 vizinhoC[2,3,1]
-                                   0                           1 
-    12 vizinhoC[3,4,2]
-                                   0                           1 
-    13 vizinhoC[3,4,1]
-                                   1                           1 
-    14 vizinhoD[1,2,3]
-                                   0                           1 
-    15 vizinhoD[2,3,4]
-                                   0                           1 
-    16 vizinhoD[1,3,4]
-                                   1                           1 
-    17 maximal[1,2]                1             1               
-    18 maximal[2,3]                1             1               
-    19 maximal[3,4]                1             1               
-    20 maximal[1,4]                1             1               
-    21 maximal[1,3]                3             1               
-    22 objetivo                    1                             
+     1 vizinhoA[1]                 0                           1 
+     2 vizinhoA[2]                 1                           1 
+     3 vizinhoA[3]                 1                           1 
+     4 vizinhoA[4]                 0                           1 
+     5 maximal[1,2]                1             1               
+     6 maximal[2,3]                1             1               
+     7 maximal[3,4]                1             1               
+     8 objetivo                    1                             
 
    No. Column name       Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 x[1,4]       *              0             0             1 
-     2 x[1,2]       *              0             0             1 
-     3 x[1,3]       *              1             0             1 
-     4 x[2,3]       *              0             0             1 
-     5 x[3,4]       *              0             0             1 
+     1 x[1,2]       *              0             0             1 
+     2 x[2,3]       *              1             0             1 
+     3 x[3,4]       *              0             0             1 
 
 Integer feasibility conditions:
 
---
+++ trunk/parte 1/mn27/mn27.sol
@@ -0,0 +1,277 @@
+Problem:    Unknown
+Rows:       127
+Columns:    56 (56 integer, 56 binary)
+Non-zeros:  294
+Status:     INTEGER OPTIMAL
+Objective:  objetivo = 3 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 unicidade[1]                1             1             = 
+     2 unicidade[2]                1             1             = 
+     3 unicidade[3]                1             1             = 
+     4 unicidade[4]                1             1             = 
+     5 unicidade[5]                1             1             = 
+     6 unicidade[6]                1             1             = 
+     7 unicidade[7]                1             1             = 
+     8 adjacencia[1,2,1]
+                                   0                           1 
+     9 adjacencia[1,2,2]
+                                   1                           1 
+    10 adjacencia[1,2,3]
+                                   1                           1 
+    11 adjacencia[1,2,4]
+                                   0                           1 
+    12 adjacencia[1,2,5]
+                                   0                           1 
+    13 adjacencia[1,2,6]
+                                   0                           1 
+    14 adjacencia[1,2,7]
+                                   0                           1 
+    15 adjacencia[1,3,1]
+                                   0                           1 
+    16 adjacencia[1,3,2]
+                                   1                           1 
+    17 adjacencia[1,3,3]
+                                   1                           1 
+    18 adjacencia[1,3,4]
+                                   0                           1 
+    19 adjacencia[1,3,5]
+                                   0                           1 
+    20 adjacencia[1,3,6]
+                                   0                           1 
+    21 adjacencia[1,3,7]
+                                   0                           1 
+    22 adjacencia[2,4,1]
+                                   0                           1 
+    23 adjacencia[2,4,2]
+                                   1                           1 
+    24 adjacencia[2,4,3]
+                                   0                           1 
+    25 adjacencia[2,4,4]
+                                   0                           1 
+    26 adjacencia[2,4,5]
+                                   0                           1 
+    27 adjacencia[2,4,6]
+                                   0                           1 
+    28 adjacencia[2,4,7]
+                                   1                           1 
+    29 adjacencia[3,4,1]
+                                   0                           1 
+    30 adjacencia[3,4,2]
+                                   1                           1 
+    31 adjacencia[3,4,3]
+                                   0                           1 
+    32 adjacencia[3,4,4]
+                                   0                           1 
+    33 adjacencia[3,4,5]
+                                   0                           1 
+    34 adjacencia[3,4,6]
+                                   0                           1 
+    35 adjacencia[3,4,7]
+                                   1                           1 
+    36 adjacencia[3,5,1]
+                                   0                           1 
+    37 adjacencia[3,5,2]
+                                   1                           1 
+    38 adjacencia[3,5,3]
+                                   1                           1 
+    39 adjacencia[3,5,4]
+                                   0                           1 
+    40 adjacencia[3,5,5]
+                                   0                           1 
+    41 adjacencia[3,5,6]
+                                   0                           1 
+    42 adjacencia[3,5,7]
+                                   0                           1 
+    43 adjacencia[4,5,1]
+                                   0                           1 
+    44 adjacencia[4,5,2]
+                                   0                           1 
+    45 adjacencia[4,5,3]
+                                   1                           1 
+    46 adjacencia[4,5,4]
+                                   0                           1 
+    47 adjacencia[4,5,5]
+                                   0                           1 
+    48 adjacencia[4,5,6]
+                                   0                           1 
+    49 adjacencia[4,5,7]
+                                   1                           1 
+    50 adjacencia[4,6,1]
+                                   0                           1 
+    51 adjacencia[4,6,2]
+                                   1                           1 
+    52 adjacencia[4,6,3]
+                                   0                           1 
+    53 adjacencia[4,6,4]
+                                   0                           1 
+    54 adjacencia[4,6,5]
+                                   0                           1 
+    55 adjacencia[4,6,6]
+                                   0                           1 
+    56 adjacencia[4,6,7]
+                                   1                           1 
+    57 adjacencia[4,7,1]
+                                   0                           1 
+    58 adjacencia[4,7,2]
+                                   0                           1 
+    59 adjacencia[4,7,3]
+                                   1                           1 
+    60 adjacencia[4,7,4]
+                                   0                           1 
+    61 adjacencia[4,7,5]
+                                   0                           1 
+    62 adjacencia[4,7,6]
+                                   0                           1 
+    63 adjacencia[4,7,7]
+                                   1                           1 
+    64 adjacencia[5,6,1]
+                                   0                           1 
+    65 adjacencia[5,6,2]
+                                   1                           1 
+    66 adjacencia[5,6,3]
+                                   1                           1 
+    67 adjacencia[5,6,4]
+                                   0                           1 
+    68 adjacencia[5,6,5]
+                                   0                           1 
+    69 adjacencia[5,6,6]
+                                   0                           1 
+    70 adjacencia[5,6,7]
+                                   0                           1 
+    71 adjacencia[6,7,1]
+                                   0                           1 
+    72 adjacencia[6,7,2]
+                                   1                           1 
+    73 adjacencia[6,7,3]
+                                   1                           1 
+    74 adjacencia[6,7,4]
+                                   0                           1 
+    75 adjacencia[6,7,5]
+                                   0                           1 
+    76 adjacencia[6,7,6]
+                                   0                           1 
+    77 adjacencia[6,7,7]
+                                   0                           1 
+    78 uso[1,1]                    0                          -0 
+    79 uso[1,2]                   -1                          -0 
+    80 uso[1,3]                    0                          -0 
+    81 uso[1,4]                    0                          -0 
+    82 uso[1,5]                    0                          -0 
+    83 uso[1,6]                    0                          -0 
+    84 uso[1,7]                   -1                          -0 
+    85 uso[2,1]                    0                          -0 
+    86 uso[2,2]                    0                          -0 
+    87 uso[2,3]                   -1                          -0 
+    88 uso[2,4]                    0                          -0 
+    89 uso[2,5]                    0                          -0 
+    90 uso[2,6]                    0                          -0 
+    91 uso[2,7]                   -1                          -0 
+    92 uso[3,1]                    0                          -0 
+    93 uso[3,2]                    0                          -0 
+    94 uso[3,3]                   -1                          -0 
+    95 uso[3,4]                    0                          -0 
+    96 uso[3,5]                    0                          -0 
+    97 uso[3,6]                    0                          -0 
+    98 uso[3,7]                   -1                          -0 
+    99 uso[4,1]                    0                          -0 
+   100 uso[4,2]                   -1                          -0 
+   101 uso[4,3]                   -1                          -0 
+   102 uso[4,4]                    0                          -0 
+   103 uso[4,5]                    0                          -0 
+   104 uso[4,6]                    0                          -0 
+   105 uso[4,7]                    0                          -0 
+   106 uso[5,1]                    0                          -0 
+   107 uso[5,2]                   -1                          -0 
+   108 uso[5,3]                    0                          -0 
+   109 uso[5,4]                    0                          -0 
+   110 uso[5,5]                    0                          -0 
+   111 uso[5,6]                    0                          -0 
+   112 uso[5,7]                   -1                          -0 
+   113 uso[6,1]                    0                          -0 
+   114 uso[6,2]                    0                          -0 
+   115 uso[6,3]                   -1                          -0 
+   116 uso[6,4]                    0                          -0 
+   117 uso[6,5]                    0                          -0 
+   118 uso[6,6]                    0                          -0 
+   119 uso[6,7]                   -1                          -0 
+   120 uso[7,1]                    0                          -0 
+   121 uso[7,2]                   -1                          -0 
+   122 uso[7,3]                    0                          -0 
+   123 uso[7,4]                    0                          -0 
+   124 uso[7,5]                    0                          -0 
+   125 uso[7,6]                    0                          -0 
+   126 uso[7,7]                   -1                          -0 
+   127 objetivo                    3                             
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 cor[1]       *              0             0             1 
+     2 cor[2]       *              1             0             1 
+     3 cor[3]       *              1             0             1 
+     4 cor[4]       *              0             0             1 
+     5 cor[5]       *              0             0             1 
+     6 cor[6]       *              0             0             1 
+     7 cor[7]       *              1             0             1 
+     8 x[1,1]       *              0             0             1 
+     9 x[1,2]       *              0             0             1 
+    10 x[1,3]       *              1             0             1 
+    11 x[1,4]       *              0             0             1 
+    12 x[1,5]       *              0             0             1 
+    13 x[1,6]       *              0             0             1 
+    14 x[1,7]       *              0             0             1 
+    15 x[2,1]       *              0             0             1 
+    16 x[2,2]       *              1             0             1 
+    17 x[2,3]       *              0             0             1 
+    18 x[2,4]       *              0             0             1 
+    19 x[2,5]       *              0             0             1 
+    20 x[2,6]       *              0             0             1 
+    21 x[2,7]       *              0             0             1 
+    22 x[3,1]       *              0             0             1 
+    23 x[3,2]       *              1             0             1 
+    24 x[3,3]       *              0             0             1 
+    25 x[3,4]       *              0             0             1 
+    26 x[3,5]       *              0             0             1 
+    27 x[3,6]       *              0             0             1 
+    28 x[3,7]       *              0             0             1 
+    29 x[4,1]       *              0             0             1 
+    30 x[4,2]       *              0             0             1 
+    31 x[4,3]       *              0             0             1 
+    32 x[4,4]       *              0             0             1 
+    33 x[4,5]       *              0             0             1 
+    34 x[4,6]       *              0             0             1 
+    35 x[4,7]       *              1             0             1 
+    36 x[5,1]       *              0             0             1 
+    37 x[5,2]       *              0             0             1 
+    38 x[5,3]       *              1             0             1 
+    39 x[5,4]       *              0             0             1 
+    40 x[5,5]       *              0             0             1 
+    41 x[5,6]       *              0             0             1 
+    42 x[5,7]       *              0             0             1 
+    43 x[6,1]       *              0             0             1 
+    44 x[6,2]       *              1             0             1 
+    45 x[6,3]       *              0             0             1 
+    46 x[6,4]       *              0             0             1 
+    47 x[6,5]       *              0             0             1 
+    48 x[6,6]       *              0             0             1 
+    49 x[6,7]       *              0             0             1 
+    50 x[7,1]       *              0             0             1 
+    51 x[7,2]       *              0             0             1 
+    52 x[7,3]       *              1             0             1 
+    53 x[7,4]       *              0             0             1 
+    54 x[7,5]       *              0             0             1 
+    55 x[7,6]       *              0             0             1 
+    56 x[7,7]       *              0             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
