7c5db9ab3f5b223723f349dfe152e38fe05a0a66
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@62949778-16d8-8ee4-2210-6f5ccdf4390f>
2011-06-16 04:53:23 +0000

    Criada uma função que testa a vizinhança de uma solução ótima e tenta encontrar soluções melhores. O algoritmo para isso é o seguinte: - Parte de uma solução ótima inicial(SI) já especificada 1 - Para cada shard lida 's' de SI, o algoritmo tira essa shard da solução, desocupando ela do satelite que ela ocupa e em seguida retirando seu ganho do valor da solução 2 - Percorre o vetor de shards, colocando no satelite desocupado por 's', todas as shards possiveis 3 - Recalcula o valor para essa troca 4 - Se o valor for maior, armazena essa solução(separada de SI) e seu valor 5 - Repete 1 (partindo sempre de SI)
    E outros refinamentos gerais no código

--- parte 2/sssp.cpp
+++ parte 2/sssp.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ struct shard{
     float rShard;        //Ganho de Armazenagem
     float cH;            //Custo de armazenagem pelo satelite em rota horizontal
     float cV;            //Custo de armazenagem pelo satelite em rota vertical
+    int nsh;
     int lidaPor;
     bool lida;
 };
@@ -33,44 +35,53 @@ bool compShard(shard shard1, shard shard2){
 
 }
 
-void build(vector<shard>& obj, vector<satelite>& sat){
-    
-    int Objetivo = 0;
+
+int build(vector<shard>& obj, vector<satelite>& sat, vector<char>& sol){
+
+    int Objetivo = 0, count = 0;
     int nSat = sat.size()/2;
-    int nSh = obj.size();
-    
+    int nSh = obj.size(), k;
+    k = 12;
+
     cout << endl << "BUILD PELOS SATELITES" << endl << endl;
 
     for(int i = 0; i < 2*nSat; i++){
         for(int j = 0; j < nSh; j++){
             if(obj[j].posH == sat[i].ns && obj[j].lida == false){
                 if(sat[i].memRestante >= obj[j].cH){
-                    cout << "Shard lida: " << obj[j].posH << " " << obj[j].posV << endl;
+                    /*cout << "Shard lida: [" << obj[j].nsh << "] Posição: "  << obj[j].posH << " " << obj[j].posV << endl;
                     cout << "Satelite leitor: " << sat[i].ns << endl;
                     cout << "Tam Horizontal: " << obj[j].cH << endl;
-                    cout << "Mem. Restante Sat Leitor: " << sat[i].memRestante << endl << endl;              
+                    cout << "Mem. Restante Sat Leitor: " << sat[i].memRestante << endl;
+                    cout << "pos. no vetor de shards: " << j << endl;*/
                     obj[j].lida = true;
                     obj[j].lidaPor = i;
                     sat[i].memRestante -= obj[j].cH;
                     Objetivo += obj[j].rShard;
+                    count += 1;
+                    sol[j] = 'h';
                 }
 	    }
-		
+
             if((obj[j].posV + nSat) == sat[i].ns && obj[j].lida == false){
                 if(sat[i].memRestante >= obj[j].cV){
-                    cout << "Shard lida: " << obj[j].posH << " " << obj[j].posV << endl;
+                    /*cout << "Shard lida: [" << obj[j].nsh << "] Posição: "  << obj[j].posH << " " << obj[j].posV << endl;
                     cout << "Satelite leitor: " << sat[i].ns << endl;
                     cout << "Tam Vertical: " << obj[j].cV << endl;
-                    cout << "Mem. Restante Sat Leitor: " << sat[i].memRestante << endl << endl;
+                    cout << "Mem. Restante Sat Leitor: " << sat[i].memRestante << endl;
+                    cout << "pos. no vetor de shards: " << j << endl;*/
                     obj[j].lida = true;
                     obj[j].lidaPor = i;
                     sat[i].memRestante -= obj[j].cV;
                     Objetivo += obj[j].rShard;
+                    count += 1;
+                    sol[j] = 'v';
 		}
 	    }
 	}  
     }
-    cout << endl << "Objetivo: " << Objetivo << endl << endl;
+    cout << endl << "Ganho Obtido: " << Objetivo << endl << "Shards Lidas: " << count << endl;
+    return Objetivo;
 
 }
 
@@ -78,9 +89,9 @@ void build(vector<shard>& obj, vector<satelite>& sat){
 /* A função abre o arquivo de entrada, lê os parâmetros necessários, montando vetores de estruturas de satelites e */ 
 /* vetores de estruturas de shards */
 
-void readIn(vector<shard>& obj, vector<satelite>& satH, vector<satelite>& satV, char* entrada){
+int readIn(vector<shard>& obj, vector<satelite>& satH, vector<char>& sol, char* entrada){
     ifstream arqin(entrada);
-    int n, i, seq;
+    int n, i, seq, total = 0;
     string tok, line;
     stringstream iss;
 
@@ -136,49 +147,110 @@ void readIn(vector<shard>& obj, vector<satelite>& satH, vector<satelite>& satV, 
         getline(iss,tok,' ');
         seq = atoi(tok.c_str());
         sh.cV = seq;
+        total += sh.rShard;
+        sh.nsh = i;
         sh.lida = false;
         sh.lidaPor = -1;
         obj.insert(obj.end(),sh);
+        sol.insert(sol.end(),'n');
     
     }
 
-    return;
+    return total;
 }
+
+int testNeighborhood(vector<shard>& obj, vector<satelite>& satH, vector<char>& solAtual, int totalAtual){
+    int nSh = obj.size();
+    int memRestAtual = 0, satLeitor = 0, novoTotal = 0, custoShard = 0, BestFit = totalAtual;
+    vector<char> novaSol = solAtual;
+
+    cout << "TESTE DE VIZINHANCA" << endl << endl;
+
+    for(int i = 0; i < nSh; i++){
+        if(obj[i].lida == true){
+            satLeitor = obj[i].lidaPor;
+            novoTotal = totalAtual - obj[i].rShard;
+            if(obj[i].lidaPor < nSh) custoShard = obj[i].cH;      //esse if-else escobre qual o custo que a shard teve 
+            else custoShard = obj[i].cV;                          //no satelite que de fato ela ocupou
+            memRestAtual = satH[satLeitor].memRestante + custoShard;
+            novaSol[i] = 'n';
+
+            for(int j = 0; j < nSh; j++){
+                //Se a shard j nao tiver sido lida, sua posH for igual ao sat. que leu a shard que saiu,
+                //ela não é a shard que saiu, e ela ocupa uma memoria menor que a restante no satelite,
+                //adicionaremos ela a nossa nova possível solução
+                if(obj[j].lida == false && obj[j].posH == satLeitor && i != j && obj[j].cH <= memRestAtual){
+                    memRestAtual -= obj[j].cH;
+                    novoTotal += obj[j].rShard;
+                    novaSol[j] = 'h';
+                    
+                }
+                //Idem, porem para posV
+                else if(obj[j].lida == false && obj[j].posV == satLeitor && i != j && obj[j].cV <= memRestAtual){
+                         memRestAtual -= obj[j].cV;
+                         novoTotal += obj[j].rShard;
+                         novaSol[j] = 'v';
+                     }
+            }
+            
+            if(novoTotal >= BestFit){
+            solAtual = novaSol;
+            BestFit = novoTotal;
+            }
+            else novaSol = solAtual;
+        
+        }
+    
+    }
+    return BestFit;
+}
+
 
 int main(int argc, char* argv[]){
     ifstream entrada;
     ofstream saida;
-    int i, n;
+    int i, n, Z, limS, nZ, tIn = time(NULL), tFin;
     vector<shard> obj;
     vector<satelite> satH;
-    vector<satelite> satV;
-    vector<int> resp;
+    vector<char> sol;
+    i = 0;
+    n = 0;
 
-    readIn(obj, satH, satV, argv[1]);
-    
+    limS = readIn(obj, satH, sol, argv[1]);
     sort(satH.begin(),satH.end(),compSat);
 
-    cout << "SATELITES: " << endl;
+    /*cout << "SATELITES: " << endl;
     n = satH.size();
     for(i=0;i<n;i++){
         cout << "satelite H " << satH[i].ns << " Pos Vetor: " << i << ": Memoria Total: " << satH[i].memTotal << endl;
     }
-    
-    cout << endl;
-    
+    cout << endl;*/
+
 
     sort(obj.begin(), obj.end(), compShard);
-    
-    n = obj.size();
+
+    /*n = obj.size();
     cout << "SHARDS: " << endl;
     for(i=0;i<n;i++){
-        cout << "Shard " << i+1 << "  Pos: " << obj[i].posH << " " << obj[i].posV << "  Ganho: " << obj[i].rShard << endl;
+        cout << "Shard " << obj[i].nsh << "  Pos: " << obj[i].posH << " " << obj[i].posV << "  Ganho: " << obj[i].rShard << endl;
         cout << "Custo H: " << obj[i].cH << " Custo V: " << obj[i].cV << endl;
         cout << "Relacao custo memoria: " << max(obj[i].rShard/obj[i].cH, obj[i].rShard/obj[i].cV) << endl << endl;
-    }
+    }*/
+    cout << endl << endl;
+    cout << "Limitante superior: " << limS << endl << endl;
+
+    Z = build(obj, satH, sol);
     cout << endl;
-    
-    build(obj, satH);
+
+    if(Z == limS){
+        cout << endl << "Solução ótima usa todas as shards!" << endl << endl;
+        return 0;
+    }
+    nZ = testNeighborhood(obj, satH, sol, Z);
+    cout << "Nova Solucao: " << nZ << endl;
+    tFin = time(NULL);
+    cout << "Tempo de execução (seg): " << difftime(tFin,tIn) << endl << endl;
+
 
     return 0;
 }
